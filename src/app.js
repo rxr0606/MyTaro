8bdc73c2cde8c4ea9bde6b70974edb28540fae52
rxr0606 <1870162490@qq.com>
2023-03-23 14:39:35 +0800

    初始化小程序

---
+++ src/app.js
@@ -0,0 +1,20 @@
+import { Component } from 'react'
+import './app.less'
+
+class App extends Component {
+
+  componentDidMount () {}
+
+  //程序/页面，启动或切前台时触发；对应小程序的onShow
+  componentDidShow () {}
+
+  //程序/页面，切后台或隐藏时触发；对应小程序的onHide
+  componentDidHide () {}
+
+  render () {
+    // this.props.children 是将要会渲染的页面
+    return this.props.children
+  }
+}
+
+export default App
